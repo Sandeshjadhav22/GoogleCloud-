fd880143ede40c697d3a875d8a392fb821feb11d
sandesh jadhav <sandeshjadhav2002@gmail.com>
2023-09-19 00:26:30 +0530

    Responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./Navbar.css";
-import logo from "./images/logo.jpg";
+import logo from "./images/logo.png";
 
 
 const Navbar = () => {
@@ -11,10 +11,10 @@ const Navbar = () => {
           <img class="logo" src={logo} alt="logo" />
         </div>
         <div class="social-icon">
-            <a class="linkedin" href="https://www.linkedin.com/company/gdsc-dmce/"><i class="fa-brands fa-linkedin " id="linkedin"></i></a>
-            <a class="linkedin"href="https://github.com/Google-Developer-Students-Club-DMCE"><i class="fa-brands fa-github " id="linkedin1"></i></a>
-            <a class="linkedin"href="https://www.instagram.com/gdsc.dmce"><i class="fa-brands fa-instagram " id="linkedin2"></i></a>
-            <a class="linkedin"href="https://www.youtube.com/@gdsc_dmce"><i class="fa-brands fa-youtube"></i></a>
+            <a target='_blank' href="https://www.linkedin.com/company/gdsc-dmce/"><i class="fa-brands fa-linkedin  linkedin" id="linkedin"></i></a>
+            <a target='_blank' href="https://github.com/Google-Developer-Students-Club-DMCE"><i class="fa-brands fa-github  linkedin" id="linkedin1"></i></a>
+            <a target='_blank' href="https://www.instagram.com/gdsc.dmce"><i class="fa-brands fa-instagram  linkedin" id="linkedin2"></i></a>
+            <a target='_blank' href="https://www.youtube.com/@gdsc_dmce"><i class="fa-brands fa-youtube linkedin"></i></a>
         </div>
       </nav>
       
