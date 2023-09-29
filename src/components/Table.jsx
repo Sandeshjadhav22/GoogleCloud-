c4c309ba9dc5b60f3fa7fea47d9856db1266ede0
omkarb81 <117004362+omkarb81@users.noreply.github.com>
2023-09-29 12:45:08 +0530

    convert csv to json data

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,12 +5,53 @@ import { useState } from 'react';
 const Table = () => {
 
     const dummyData = [
-        {id: 1, NAME: 'Manas Kulkarni', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'not completed'},
-         {id: 2, NAME: 'Sandesh Jadhav', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'completed '},
-         {id: 3, NAME: 'Viraj Sharma', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'completed'},
-        {id: 4, NAME: 'Rohit Koli', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'not completed'},
-         {id: 5, NAME: 'Akkash Chopra', COURSES: 4, SKILL: 4, GEN_AI: 1, DUE_DATE:'18/10/23', STATUS: 'not completed'},
-       ]
+        {
+          id: 1,
+          NAME: 'Manas Kulkarni',
+          COURSES: 4,
+          SKILL: 4,
+          GEN_AI: 1,
+          DUE_DATE: '18/10/23',
+          STATUS: 'not completed',
+        },
+        {
+          id: 2,
+          NAME: 'Sandesh Jadhav',
+          COURSES: 4,
+          SKILL: 4,
+          GEN_AI: 1,
+          DUE_DATE: '18/10/23',
+          STATUS: 'completed',
+        },
+        {
+          id: 3,
+          NAME: 'Viraj Sharma',
+          COURSES: 4,
+          SKILL: 4,
+          GEN_AI: 1,
+          DUE_DATE: '18/10/23',
+          STATUS: 'completed',
+        },
+        {
+          id: 4,
+          NAME: 'Rohit Koli',
+          COURSES: 4,
+          SKILL: 4,
+          GEN_AI: 1,
+          DUE_DATE: '18/10/23',
+          STATUS: 'not completed',
+        },
+        {
+          id: 5,
+          NAME: 'Akkash Chopra',
+          COURSES: 4,
+          SKILL: 4,
+          GEN_AI: 1,
+          DUE_DATE: '18/10/23',
+          STATUS: 'not completed',
+        },
+      ];
+      
 
        const [value,setValue] = useState('');
        const [dataSource,setDataSource] = useState(dummyData);
